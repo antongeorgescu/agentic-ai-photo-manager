a318dfab7cd42060a529bb3bcccd3b6d4aa6d393
Anton-Georgescu_finastra <anton.georgescu@dh.com>
2025-06-14 14:55:41 -0400

    Created a plugins folder

--- src/agent_collaborate.py
+++ src/agent_collaborate.py
@@ -16,6 +16,9 @@ from semantic_kernel.contents.chat_message_content import ChatMessageContent
 from semantic_kernel.contents.utils.author_role import AuthorRole
 from semantic_kernel.functions.kernel_function_decorator import kernel_function
 
+from agent_plugin.DevOpsPlugin import DevopsPlugin
+from agent_plugin.LogFilePlugin import LogFilePlugin
+
 PHOTO_ORGANIZER = "Photo_Organizer"
 PHOTO_ORGANIZER_INSTRUCTIONS = """
 Analyze the given log file or the response from the devops assistant.
@@ -168,8 +171,6 @@ async def main():
                 else:
                     break
 
-
-
 # class for selection strategy
 class SelectionStrategy(SequentialSelectionStrategy):
     """A strategy for determining which agent should take the next turn in the chat."""
@@ -194,88 +195,6 @@ class ApprovalTerminationStrategy(TerminationStrategy):
     async def should_agent_terminate(self, agent, history):
         """Check if the agent should terminate."""
         return "no action needed" in history[-1].content.lower()
-
-# class for DevOps functions
-class DevopsPlugin:
-    """A plugin that performs developer operation tasks."""
-    
-    def append_to_log_file(self, filepath: str, content: str) -> None:
-        with open(filepath, 'a', encoding='utf-8') as file:
-            file.write('\n' + textwrap.dedent(content).strip())
-
-    @kernel_function(description="A function that restarts the named service")
-    def restart_service(self, service_name: str = "", logfile: str = "") -> str:
-        log_entries = [
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ALERT  DevopsAssistant: Multiple failures detected in {service_name}. Restarting service.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO  {service_name}: Restart initiated.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO  {service_name}: Service restarted successfully.",
-        ]
-
-        log_message = "\n".join(log_entries)
-        self.append_to_log_file(logfile, log_message)
-        
-        return f"Service {service_name} restarted successfully."
-
-    @kernel_function(description="A function that rollsback the transaction")
-    def rollback_transaction(self, logfile: str = "") -> str:
-        log_entries = [
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ALERT  DevopsAssistant: Transaction failure detected. Rolling back transaction batch.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO   TransactionProcessor: Rolling back transaction batch.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO   Transaction rollback completed successfully.",
-        ]
-
-        log_message = "\n".join(log_entries)
-        self.append_to_log_file(logfile, log_message)
-        
-        return "Transaction rolled back successfully."
-
-    @kernel_function(description="A function that redeploys the named resource")
-    def redeploy_resource(self, resource_name: str = "", logfile: str = "") -> str:
-        log_entries = [
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ALERT  DevopsAssistant: Resource deployment failure detected in '{resource_name}'. Redeploying resource.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO   DeploymentManager: Redeployment request submitted.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO   DeploymentManager: Service successfully redeployed, resource '{resource_name}' created successfully.",
-        ]
-
-        log_message = "\n".join(log_entries)
-        self.append_to_log_file(logfile, log_message)
-        
-        return f"Resource '{resource_name}' redeployed successfully."
-
-    @kernel_function(description="A function that increases the quota")
-    def increase_quota(self, logfile: str = "") -> str:
-        log_entries = [
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ALERT  DevopsAssistant: High request volume detected. Increasing quota.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO   APIManager: Quota increase request submitted.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] INFO   APIManager: Quota successfully increased to 150% of previous limit.",
-        ]
-
-        log_message = "\n".join(log_entries)
-        self.append_to_log_file(logfile, log_message)
-
-        return "Successfully increased quota."
-
-    @kernel_function(description="A function that escalates the issue")
-    def escalate_issue(self, logfile: str = "") -> str:
-        log_entries = [
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ALERT  DevopsAssistant: Cannot resolve issue.",
-            f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] ALERT  DevopsAssistant: Requesting escalation.",
-        ]
-        
-        log_message = "\n".join(log_entries)
-        self.append_to_log_file(logfile, log_message)
-        
-        return "Submitted escalation request."
-
-
-# class for Log File functions
-class LogFilePlugin:
-    """A plugin that reads and writes log files."""
-
-    @kernel_function(description="Accesses the given file path string and returns the file contents as a string")
-    def read_log_file(self, filepath: str = "") -> str:
-        with open(filepath, 'r', encoding='utf-8') as file:
-            return file.read()
 
 # Start the app
 if __name__ == "__main__":
